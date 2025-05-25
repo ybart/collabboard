770eca4a7403f10c5d335121fca69ac583725c0a
Yvan BARTHÉLEMY <contact.ilyba@gmail.com>
2025-05-25 15:33:38 +0200

    config: add Action Cable origins for development (follow-up to 0de048a)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,6 +14,8 @@ Rails.application.configure do
 
   # Enable server timing.
   config.server_timing = true
+  config.action_cable.allowed_request_origins = /(\.dev$)|^localhost$/
+  config.hosts << "collabboard.tpk.dev"
 
   # Enable/disable Action Controller caching. By default Action Controller caching is disabled.
   # Run rails dev:cache to toggle Action Controller caching.
